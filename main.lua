81ccbcfe9fff4db812ca5331af94693cc9f55892
Howl <93114188+IOnlySpeakMexican@users.noreply.github.com>
2022-06-16 00:20:07 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,3 @@
--- Gui to Lua
--- Version: 3.2
-
--- Instances:
 
 local ScreenGui = Instance.new("ScreenGui")
 local automation = Instance.new("TextLabel")
@@ -18,9 +14,8 @@ local comingsoon = Instance.new("TextButton")
 local togg_4 = Instance.new("Frame")
 local comingsoon_2 = Instance.new("TextButton")
 local togg_5 = Instance.new("Frame")
-local TextLabel = Instance.new("TextLabel")
 local minimise = Instance.new("TextButton")
-local EggTab = Instance.new("TextLabel")
+local eggtab = Instance.new("TextLabel")
 local EGGBCG = Instance.new("Frame")
 local toggegab = Instance.new("TextButton")
 local togg_6 = Instance.new("Frame")
@@ -30,17 +25,25 @@ local eggamount = Instance.new("TextBox")
 local minimiseEGG = Instance.new("TextButton")
 local teleporttab = Instance.new("TextLabel")
 local TELEBCG = Instance.new("Frame")
-local antikick = Instance.new("TextButton")
 local Shops = Instance.new("TextButton")
 local Portals = Instance.new("TextButton")
 local Leaderboards = Instance.new("TextButton")
+local Lava = Instance.new("TextButton")
+local Aqua = Instance.new("TextButton")
+local Sakura = Instance.new("TextButton")
+local Forest = Instance.new("TextButton")
+local Desert = Instance.new("TextButton")
+local Winter = Instance.new("TextButton")
+local Mines = Instance.new("TextButton")
+local Galaxy = Instance.new("TextButton")
+local Heaven = Instance.new("TextButton")
+local Hell = Instance.new("TextButton")
 local minimiseTELE = Instance.new("TextButton")
 local rebirthvall = Instance.new("NumberValue")
 local worlforpawcoin = Instance.new("StringValue")
 local eggtobuy = Instance.new("StringValue")
 local eggamountt = Instance.new("NumberValue")
 
---Properties:
 
 ScreenGui.Parent = game.CoreGui
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
@@ -200,16 +203,6 @@ togg_5.BorderSizePixel = 2
 togg_5.Position = UDim2.new(0.901162922, 0, 0.291666657, 0)
 togg_5.Size = UDim2.new(0, 10, 0, 10)
 
-TextLabel.Parent = BCG
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 4.000
-TextLabel.Position = UDim2.new(0.046632126, 0, 0.929166675, 0)
-TextLabel.Size = UDim2.new(0, 174, 0, 11)
-TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "Created by: Howl#3275"
-TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.TextSize = 11.000
-
 minimise.Name = "minimise"
 minimise.Parent = automation
 minimise.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -221,22 +214,22 @@ minimise.Text = "▲"
 minimise.TextColor3 = Color3.fromRGB(255, 255, 255)
 minimise.TextSize = 14.000
 
-EggTab.Name = "EggTab"
-EggTab.Parent = ScreenGui
-EggTab.BackgroundColor3 = Color3.fromRGB(47, 49, 54)
-EggTab.BorderSizePixel = 0
-EggTab.Position = UDim2.new(0.201212123, 0, 0.103067487, 0)
-EggTab.Size = UDim2.new(0, 193, 0, 31)
-EggTab.Font = Enum.Font.GothamBold
-EggTab.Text = "  Auto Egg Tab"
-EggTab.TextColor3 = Color3.fromRGB(255, 255, 255)
-EggTab.TextSize = 10.000
-EggTab.TextXAlignment = Enum.TextXAlignment.Left
-EggTab.Active = true
-EggTab.Draggable = true
+eggtab.Name = "eggtab"
+eggtab.Parent = ScreenGui
+eggtab.BackgroundColor3 = Color3.fromRGB(47, 49, 54)
+eggtab.BorderSizePixel = 0
+eggtab.Position = UDim2.new(0.201212123, 0, 0.103067487, 0)
+eggtab.Size = UDim2.new(0, 193, 0, 31)
+eggtab.Font = Enum.Font.GothamBold
+eggtab.Text = "  Auto Egg Tab"
+eggtab.TextColor3 = Color3.fromRGB(255, 255, 255)
+eggtab.TextSize = 10.000
+eggtab.TextXAlignment = Enum.TextXAlignment.Left
+eggtab.Active = true
+eggtab.Draggable = true
 
 EGGBCG.Name = "EGGBCG"
-EGGBCG.Parent = EggTab
+EGGBCG.Parent = eggtab
 EGGBCG.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
 EGGBCG.BorderSizePixel = 0
 EGGBCG.Position = UDim2.new(0, 0, 0.99841702, 0)
@@ -285,7 +278,7 @@ antiteleportkick.BorderSizePixel = 2
 antiteleportkick.Position = UDim2.new(0.0569947399, 0, 0.0754172653, 0)
 antiteleportkick.Size = UDim2.new(0, 172, 0, 24)
 antiteleportkick.Font = Enum.Font.GothamMedium
-antiteleportkick.Text = " Anti Kick (Use if new)"
+antiteleportkick.Text = " Anti Kick (Use If New)"
 antiteleportkick.TextColor3 = Color3.fromRGB(255, 255, 255)
 antiteleportkick.TextSize = 9.000
 antiteleportkick.TextXAlignment = Enum.TextXAlignment.Left
@@ -305,7 +298,7 @@ eggamount.TextColor3 = Color3.fromRGB(255, 255, 255)
 eggamount.TextSize = 9.000
 
 minimiseEGG.Name = "minimiseEGG"
-minimiseEGG.Parent = EggTab
+minimiseEGG.Parent = eggtab
 minimiseEGG.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 minimiseEGG.BackgroundTransparency = 4.000
 minimiseEGG.Position = UDim2.new(0.854922295, 0, 0.225806445, 0)
@@ -334,27 +327,14 @@ TELEBCG.Parent = teleporttab
 TELEBCG.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
 TELEBCG.BorderSizePixel = 0
 TELEBCG.Position = UDim2.new(0, 0, 0.99841702, 0)
-TELEBCG.Size = UDim2.new(0, 193, 0, 266)
-
-antikick.Name = "antikick"
-antikick.Parent = TELEBCG
-antikick.BackgroundColor3 = Color3.fromRGB(54, 57, 63)
-antikick.BorderColor3 = Color3.fromRGB(47, 49, 54)
-antikick.BorderSizePixel = 2
-antikick.Position = UDim2.new(0.0569947399, 0, 0.0378232822, 0)
-antikick.Size = UDim2.new(0, 172, 0, 24)
-antikick.Font = Enum.Font.GothamMedium
-antikick.Text = "  Coming soon"
-antikick.TextColor3 = Color3.fromRGB(255, 255, 255)
-antikick.TextSize = 9.000
-antikick.TextXAlignment = Enum.TextXAlignment.Left
+TELEBCG.Size = UDim2.new(0, 193, 0, 410)
 
 Shops.Name = "Shops"
 Shops.Parent = TELEBCG
 Shops.BackgroundColor3 = Color3.fromRGB(54, 57, 63)
 Shops.BorderColor3 = Color3.fromRGB(47, 49, 54)
 Shops.BorderSizePixel = 2
-Shops.Position = UDim2.new(0.0569947399, 0, 0.0379999988, 30)
+Shops.Position = UDim2.new(0.0518133938, 0, -0.0500410125, 30)
 Shops.Size = UDim2.new(0, 172, 0, 24)
 Shops.Font = Enum.Font.GothamMedium
 Shops.Text = "  Shops"
@@ -367,7 +347,7 @@ Portals.Parent = TELEBCG
 Portals.BackgroundColor3 = Color3.fromRGB(54, 57, 63)
 Portals.BorderColor3 = Color3.fromRGB(47, 49, 54)
 Portals.BorderSizePixel = 2
-Portals.Position = UDim2.new(0.0569947399, 0, 0.0379999988, 60)
+Portals.Position = UDim2.new(0.0518133938, 0, -0.0500410125, 60)
 Portals.Size = UDim2.new(0, 172, 0, 24)
 Portals.Font = Enum.Font.GothamMedium
 Portals.Text = "  Portals"
@@ -380,13 +360,143 @@ Leaderboards.Parent = TELEBCG
 Leaderboards.BackgroundColor3 = Color3.fromRGB(54, 57, 63)
 Leaderboards.BorderColor3 = Color3.fromRGB(47, 49, 54)
 Leaderboards.BorderSizePixel = 2
-Leaderboards.Position = UDim2.new(0.0569947399, 0, 0.0379999988, 90)
+Leaderboards.Position = UDim2.new(0.0518133938, 0, -0.0500410125, 90)
 Leaderboards.Size = UDim2.new(0, 172, 0, 24)
 Leaderboards.Font = Enum.Font.GothamMedium
 Leaderboards.Text = "  Leaderboards"
 Leaderboards.TextColor3 = Color3.fromRGB(255, 255, 255)
 Leaderboards.TextSize = 9.000
 Leaderboards.TextXAlignment = Enum.TextXAlignment.Left
+
+Lava.Name = "Lava"
+Lava.Parent = TELEBCG
+Lava.BackgroundColor3 = Color3.fromRGB(54, 57, 63)
+Lava.BorderColor3 = Color3.fromRGB(47, 49, 54)
+Lava.BorderSizePixel = 2
+Lava.Position = UDim2.new(0.0518133938, 0, -0.0500410125, 120)
+Lava.Size = UDim2.new(0, 172, 0, 24)
+Lava.Font = Enum.Font.GothamMedium
+Lava.Text = "  Lava"
+Lava.TextColor3 = Color3.fromRGB(255, 255, 255)
+Lava.TextSize = 9.000
+Lava.TextXAlignment = Enum.TextXAlignment.Left
+
+Aqua.Name = "Aqua"
+Aqua.Parent = TELEBCG
+Aqua.BackgroundColor3 = Color3.fromRGB(54, 57, 63)
+Aqua.BorderColor3 = Color3.fromRGB(47, 49, 54)
+Aqua.BorderSizePixel = 2
+Aqua.Position = UDim2.new(0.0518133938, 0, -0.0499912836, 150)
+Aqua.Size = UDim2.new(0, 172, 0, 24)
+Aqua.Font = Enum.Font.GothamMedium
+Aqua.Text = "  Aqua"
+Aqua.TextColor3 = Color3.fromRGB(255, 255, 255)
+Aqua.TextSize = 9.000
+Aqua.TextXAlignment = Enum.TextXAlignment.Left
+
+Sakura.Name = "Sakura"
+Sakura.Parent = TELEBCG
+Sakura.BackgroundColor3 = Color3.fromRGB(54, 57, 63)
+Sakura.BorderColor3 = Color3.fromRGB(47, 49, 54)
+Sakura.BorderSizePixel = 2
+Sakura.Position = UDim2.new(0.0518133938, 0, -0.0499912836, 180)
+Sakura.Size = UDim2.new(0, 172, 0, 24)
+Sakura.Font = Enum.Font.GothamMedium
+Sakura.Text = "  Sakura"
+Sakura.TextColor3 = Color3.fromRGB(255, 255, 255)
+Sakura.TextSize = 9.000
+Sakura.TextXAlignment = Enum.TextXAlignment.Left
+
+Forest.Name = "Forest"
+Forest.Parent = TELEBCG
+Forest.BackgroundColor3 = Color3.fromRGB(54, 57, 63)
+Forest.BorderColor3 = Color3.fromRGB(47, 49, 54)
+Forest.BorderSizePixel = 2
+Forest.Position = UDim2.new(0.0518133938, 0, -0.0499912836, 210)
+Forest.Size = UDim2.new(0, 172, 0, 24)
+Forest.Font = Enum.Font.GothamMedium
+Forest.Text = "  Forest"
+Forest.TextColor3 = Color3.fromRGB(255, 255, 255)
+Forest.TextSize = 9.000
+Forest.TextXAlignment = Enum.TextXAlignment.Left
+
+Desert.Name = "Desert"
+Desert.Parent = TELEBCG
+Desert.BackgroundColor3 = Color3.fromRGB(54, 57, 63)
+Desert.BorderColor3 = Color3.fromRGB(47, 49, 54)
+Desert.BorderSizePixel = 2
+Desert.Position = UDim2.new(0.0518133938, 0, -0.0499912836, 240)
+Desert.Size = UDim2.new(0, 172, 0, 24)
+Desert.Font = Enum.Font.GothamMedium
+Desert.Text = "  Desert"
+Desert.TextColor3 = Color3.fromRGB(255, 255, 255)
+Desert.TextSize = 9.000
+Desert.TextXAlignment = Enum.TextXAlignment.Left
+
+Winter.Name = "Winter"
+Winter.Parent = TELEBCG
+Winter.BackgroundColor3 = Color3.fromRGB(54, 57, 63)
+Winter.BorderColor3 = Color3.fromRGB(47, 49, 54)
+Winter.BorderSizePixel = 2
+Winter.Position = UDim2.new(0.0518133938, 0, -0.0499912836, 270)
+Winter.Size = UDim2.new(0, 172, 0, 24)
+Winter.Font = Enum.Font.GothamMedium
+Winter.Text = "  Winter"
+Winter.TextColor3 = Color3.fromRGB(255, 255, 255)
+Winter.TextSize = 9.000
+Winter.TextXAlignment = Enum.TextXAlignment.Left
+
+Mines.Name = "Mines"
+Mines.Parent = TELEBCG
+Mines.BackgroundColor3 = Color3.fromRGB(54, 57, 63)
+Mines.BorderColor3 = Color3.fromRGB(47, 49, 54)
+Mines.BorderSizePixel = 2
+Mines.Position = UDim2.new(0.0518133938, 0, -0.0499912836, 300)
+Mines.Size = UDim2.new(0, 172, 0, 24)
+Mines.Font = Enum.Font.GothamMedium
+Mines.Text = "  Mines"
+Mines.TextColor3 = Color3.fromRGB(255, 255, 255)
+Mines.TextSize = 9.000
+Mines.TextXAlignment = Enum.TextXAlignment.Left
+
+Galaxy.Name = "Galaxy"
+Galaxy.Parent = TELEBCG
+Galaxy.BackgroundColor3 = Color3.fromRGB(54, 57, 63)
+Galaxy.BorderColor3 = Color3.fromRGB(47, 49, 54)
+Galaxy.BorderSizePixel = 2
+Galaxy.Position = UDim2.new(0.0518133938, 0, -0.0499912836, 330)
+Galaxy.Size = UDim2.new(0, 172, 0, 24)
+Galaxy.Font = Enum.Font.GothamMedium
+Galaxy.Text = "  Galaxy"
+Galaxy.TextColor3 = Color3.fromRGB(255, 255, 255)
+Galaxy.TextSize = 9.000
+Galaxy.TextXAlignment = Enum.TextXAlignment.Left
+
+Heaven.Name = "Heaven"
+Heaven.Parent = TELEBCG
+Heaven.BackgroundColor3 = Color3.fromRGB(54, 57, 63)
+Heaven.BorderColor3 = Color3.fromRGB(47, 49, 54)
+Heaven.BorderSizePixel = 2
+Heaven.Position = UDim2.new(0.0518133938, 0, -0.0495816469, 360)
+Heaven.Size = UDim2.new(0, 172, 0, 24)
+Heaven.Font = Enum.Font.GothamMedium
+Heaven.Text = "  Heaven"
+Heaven.TextColor3 = Color3.fromRGB(255, 255, 255)
+Heaven.TextSize = 9.000
+Heaven.TextXAlignment = Enum.TextXAlignment.Left
+
+Hell.Name = "Hell"
+Hell.Parent = TELEBCG
+Hell.BackgroundColor3 = Color3.fromRGB(54, 57, 63)
+Hell.BorderColor3 = Color3.fromRGB(47, 49, 54)
+Hell.BorderSizePixel = 2
+Hell.Position = UDim2.new(0.0518133938, 0, -0.0495816469, 390)
+Hell.Size = UDim2.new(0, 172, 0, 24)
+Hell.Font = Enum.Font.GothamMedium
+Hell.Text = "  Hell"
+Hell.TextColor3 = Color3.fromRGB(255, 255, 255)
+Hell.TextSize = 9.000
+Hell.TextXAlignment = Enum.TextXAlignment.Left
 
 minimiseTELE.Name = "minimiseTELE"
 minimiseTELE.Parent = teleporttab
@@ -465,19 +575,38 @@ local thing2 = true
 minimiseTELE.MouseButton1Up:Connect(function()
 	if thing2 == false then
 		thing2 = true
-		TELEBCG:TweenSize(UDim2.new(0, 193,0, 266), 1)
+		TELEBCG:TweenSize(UDim2.new(0, 193,0, 410), 1)
 		minimiseTELE.Text = "▲"
-		wait(0.5)
+		wait(0.7)
         Leaderboards.Visible = true
         Portals.Visible = true
-        antikick.Visible = true
         Shops.Visible = true
+        Lava.Visible = true
+        Aqua.Visible = true
+        Sakura.Visible = true
+        Forest.Visible = true
+        Desert.Visible = true
+        Winter.Visible = true
+        Mines.Visible = true
+        Galaxy.Visible = true
+        Heaven.Visible = true
+        Hell.Visible = true
+
 	else
 		thing2 = false
         Leaderboards.Visible = false
         Portals.Visible = false
-        antikick.Visible = false
         Shops.Visible = false
+        Lava.Visible = false
+        Aqua.Visible = false
+        Sakura.Visible = false
+        Forest.Visible = false
+        Desert.Visible = false
+        Winter.Visible = false
+        Mines.Visible = false
+        Galaxy.Visible = false
+        Heaven.Visible = false
+        Hell.Visible = false
 		wait(0.1)
 		TELEBCG:TweenSize(UDim2.new(0, 193,0, 0), 1)
 		minimiseTELE.Text = "▼"
@@ -535,7 +664,7 @@ togar.MouseButton1Up:Connect(function()
 	end
     while _G.ToggleAR do
         wait()
-        game:GetService("ReplicatedStorage").Remotes.Rebirth:FireServer(value.Value)
+        game:GetService("ReplicatedStorage").Remotes.Rebirth:FireServer(rebirthvall.Value)
     end
 end)
 
@@ -609,4 +738,33 @@ end)
 Leaderboards.MouseButton1Up:Connect(function()
     game.Players.LocalPlayer.Character:MoveTo(Vector3.new(106.801163, 145.788605, -674.791626, -0.188097775, -6.41193552e-08, -0.982150316, 3.93399402e-09, 1, -6.60380906e-08, 0.982150316, -1.62853908e-08, -0.188097775))
 end)
-
+Lava.MouseButton1Up:Connect(function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(12.2003708, 2640.78784, -904.967224, 1, 0, 0, 0, 1, 0, 0, 0, 1))
+end)
+Aqua.MouseButton1Up:Connect(function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-70.656189, 3168.53687, -891.329346, -0.309983492, 4.99805841e-09, -0.950741947, 2.88298552e-10, 1, 5.16301046e-09, 0.950741947, 1.32635047e-09, -0.309983492))
+end)
+Sakura.MouseButton1Up:Connect(function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-11.3723364, 3646.82935, -939.349426, -0.997513533, 1.30430644e-08, 0.0704755262, 9.5331556e-09, 1, -5.01395654e-08, -0.0704755262, -4.93430399e-08, -0.997513533))
+end)
+Forest.MouseButton1Up:Connect(function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-5.04190254, 801.188965, -854.687012, 0.999988616, 2.89007964e-08, -0.00477003539, -2.91320301e-08, 1, -4.84069353e-08, 0.00477003539, 4.85453455e-08, 0.999988616))
+end)
+Desert.MouseButton1Up:Connect(function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-14.5430412, 1386.81677, -815.940002, 1, 0, 0, 0, 1, 0, 0, 0, 1))
+end)
+Winter.MouseButton1Up:Connect(function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(16.681139, 2103.08813, -874.174133, 0.0334900841, 4.45361685e-08, 0.999439061, 8.48673842e-09, 1, -4.48455459e-08, -0.999439061, 9.98385818e-09, 0.0334900841))
+end)
+Mines.MouseButton1Up:Connect(function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-20.2753639, 4104.56104, -958.358521, 1, 0, 0, 0, 1, 0, 0, 0, 1))
+end)
+Heaven.MouseButton1Up:Connect(function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(107.051239, 5003.98828, -856.317688, 0.978144467, -0, -0.207926437, 0, 1, -0, 0.207926437, 0, 0.978144467))
+end)
+Hell.MouseButton1Up:Connect(function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(83.3568344, 5464.58984, -853.871582, 0.970287263, -0, -0.241955817, 0, 1, -0, 0.241955817, 0, 0.970287263))
+end)
+Galaxy.MouseButton1Up:Connect(function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-19.5713711, 4543.97266, -804.965698, 1, 0, 0, 0, 1, 0, 0, 0, 1))
+end)
